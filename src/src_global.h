0235ce45b4430686a486796a1584300d2814f14a
amreo <amreo@live.it>
2015-10-29 22:40:37 +0100

    FOR/FORXY defines    * FOR(var,from,to) repeat from n to n for var v, ex FOR(x, 0, 20)    * FORXY(varX, fromX, toX, varY, fromY, toY) repeat ex. FOR(x, 0, 20) in FOR(y,0,10)

--- src/src_global.h
+++ src/src_global.h
@@ -9,4 +9,14 @@
 #  define SRCSHARED_EXPORT Q_DECL_IMPORT
 #endif
 
+/**
+ * Repeater a variables x and y from fromX/fromY to toX/toY
+ */
+#define FORXY(varX, fromX, toX, varY, fromY, toY) FOR(varY, fromY, toY) FOR(varX, fromX, toX)
+/**
+ * Repeat n times for variables var from expression to expression
+ */
+#define FOR(var, from, to) for(int var = from; from<=to; var++)
+
+
 #endif // SRC_GLOBAL_H
