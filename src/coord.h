9cd1ac275e005fbd6bfc5b71cad8e8ac918166a9
amreo <amreo@live.it>
2016-01-20 21:57:50 +0100

    Add documentation for Coord and costructor from QPoint

--- src/coord.h
+++ src/coord.h
@@ -1,31 +1,69 @@
 #ifndef COORD_H
 #define COORD_H
 #include "movable.h"
+#include <QPoint>
 
+/**
+ * @brief This class rappresent a 2D coord
+ * @author amreo
+ * @version 0.2
+ * @since 0.1
+ */
 class Coord : public Movable
 {
-    Q_OBJECT
-public:
-    Coord(int x = 0, int y = 0) : Movable(x,y) {}
-    Coord(const Coord& c) : Coord(c.x(), c.y()) {}
-    Coord(const AbstractLocated& c) : Coord(c.x(), c.y()) {}
+        Q_OBJECT
+    public:
+        /**
+         * @brief Costructor of coord
+         * @param Coordinate X
+         * @param Coordinate Y
+         * @param Parent object
+         */
+        Coord(int x = 0, int y = 0) : Movable(x,y) {}
+        /**
+         * @brief Copy costructor of coord
+         * @param location
+         */
+        Coord(const Coord& c) : Coord(c.x(), c.y()) {}
+        /**
+         * @brief Create a new coord from a AbstractLocated
+         * @param location
+         */
+        Coord(const AbstractLocated& c) : Coord(c.x(), c.y()) {}
+        /**
+         * @brief Costructor of coord
+         * @param point
+         */
+        explicit Coord(const QPoint& point) : Coord(point.x(), point.y()) {}
 
-    static Coord sum(const AbstractLocated& loc1, const AbstractLocated& loc2)
-    { return Coord(loc1.x()+loc2.x(), loc1.y() + loc2.y()); }
+        /**
+         * @brief Sum the left and the right
+         * @param left
+         * @param right
+         * @return the sum of left and right
+         */
+        inline static Coord sum(const AbstractLocated& left, const AbstractLocated& right)
+        { return Coord(left.x()+right.x(), left.y() + right.y()); }
 
-    static Coord sub(const AbstractLocated& loc1, const AbstractLocated& loc2)
-    { return Coord(loc1.x() - loc2.x(), loc1.y() - loc2.y()); }
+        /**
+         * @brief Sub the left and the right
+         * @param left
+         * @param right
+         * @return the sum of left and -right
+         */
+        inline static Coord sub(const AbstractLocated& left, const AbstractLocated& right)
+        { return Coord(left.x() - right.x(), left.y() - right.y()); }
 
-    Coord operator+(const AbstractLocated& loc2) const
-    { return Coord(this->x()+loc2.x(), this->y()+loc2.y()); }
-    Coord operator-(const AbstractLocated& loc2) const
-    { return Coord(this->x()-loc2.x(), this->y()-loc2.y()); }
+        inline Coord operator+(const AbstractLocated& loc2) const
+        { return Coord(this->x()+loc2.x(), this->y()+loc2.y()); }
+        inline Coord operator-(const AbstractLocated& loc2) const
+        { return Coord(this->x()-loc2.x(), this->y()-loc2.y()); }
 
-    ~Coord() {}
+        ~Coord() {}
 
-signals:
+    signals:
 
-public slots:
+    public slots:
 
 };
 
